b4a6dd1ba904ebb7aff879514cd445ef82767c89
haseeb <haseeeb8j@gmail.com>
2026-09-21 21:18:51 +0500

    updated: array define macros

--- lib/array.h
+++ lib/array.h
@@ -82,39 +82,38 @@ _mw_array_set(_mw_array* array, unsigned int i, void* val) {
 }
 
 
-#define _mw_define_internal_array(PREFIX, NAME, TYPE) \
-typedef struct { TYPE* data; unsigned int size; unsigned int elem_size; } ## PREFIX ## mw_ ## NAME ## _array; \
+#define _mw_define_array_with_prefix(PREFIX, NAME, TYPE) \
+typedef struct { TYPE* data; unsigned int size; unsigned int elem_size; } PREFIX ## mw_ ## NAME ## _array; \
 \
-static inline ## PREFIX ## mw_ ## NAME ## _array \
-## PREFIX ## mw_ ## NAME ## _array_new(unsigned int size) { \
-	_mw_array raw = _mw_array_new(size, sizeof(TYPE)); \
-	## PREFIX ## mw_ ## NAME ## _array arr = { \
-		.data = (TYPE*)raw.data, \
-		.size = raw.size, \
-		.elem_size = raw.elem_size \
-	}; \
-	return arr; \
+static inline PREFIX ## mw_ ## NAME ## _array \
+PREFIX ## mw_ ## NAME ## _array_new(unsigned int size) { \
+    _mw_array raw = _mw_array_new(size, sizeof(TYPE)); \
+    PREFIX ## mw_ ## NAME ## _array arr = { \
+        .data = (TYPE*)raw.data, \
+        .size = raw.size, \
+        .elem_size = raw.elem_size \
+    }; \
+    return arr; \
 } \
 static inline mw_code \
-## PREFIX ## mw_ ## NAME ## _array_from(## PREFIX ## mw_ ## NAME ## _array* arr, void* data) { return _mw_array_from((_mw_array*)arr, data); } \
-\
+PREFIX ## mw_ ## NAME ## _array_from(PREFIX ## mw_ ## NAME ## _array* arr, void* data) { return _mw_array_from((_mw_array*)arr, data); } \
 \
 static inline mw_code \
-## PREFIX ## mw_ ## NAME ## _array_delete(## PREFIX ## mw_ ## NAME ## _array* arr) { return _mw_array_delete((_mw_array*)arr); } \
-\
+PREFIX ## mw_ ## NAME ## _array_delete(PREFIX ## mw_ ## NAME ## _array* arr) { return _mw_array_delete((_mw_array*)arr); } \
 \
 static inline unsigned int \
-## PREFIX ## mw_ ## NAME ## _array_size(## PREFIX ## mw_ ## NAME ## _array* arr) { return arr->size; } \
+PREFIX ## mw_ ## NAME ## _array_size(PREFIX ## mw_ ## NAME ## _array* arr) { return arr->size; } \
 \
 static inline mw_code \
-## PREFIX ## mw_ ## NAME ## _array_get(## PREFIX ## mw_ ## NAME ## _array* arr, unsigned int i, TYPE* ret) { \
-	return _mw_array_get((_mw_array*)arr, i, ret); \
+PREFIX ## mw_ ## NAME ## _array_get(PREFIX ## mw_ ## NAME ## _array* arr, unsigned int i, TYPE* ret) { \
+    return _mw_array_get((_mw_array*)arr, i, ret); \
 } \
 \
 static inline mw_code \
-## PREFIX ## mw_ ## NAME ## _array_set(## PREFIX ## mw_ ## NAME ## _array* arr, unsigned int i, TYPE val) { \
-	return _mw_array_set((_mw_array*)arr, i, &val); \
-} \
+PREFIX ## mw_ ## NAME ## _array_set(PREFIX ## mw_ ## NAME ## _array* arr, unsigned int i, TYPE val) { \
+    return _mw_array_set((_mw_array*)arr, i, &val); \
+}
 
 
-#define mw_define_array(NAME, TYPE) _mw_define_internal_array(,NAME,TYPE)
+#define mw_define_array(NAME, TYPE) _mw_define_array_with_prefix(,NAME,TYPE)
+#define _mw_define_internal_array(NAME, TYPE) _mw_define_array_with_prefix(_,NAME,TYPE)

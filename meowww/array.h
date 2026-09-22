9278af9a464e0b6afa5d008ead4a1541e2289fe7
haseeb <haseeeb8j@gmail.com>
2026-09-22 23:06:51 +0500

    added: array newl method; int and float arrays provided by default

--- meowww/array.h
+++ meowww/array.h
@@ -100,8 +100,15 @@ PREFIX ## mw_ ## NAME ## _array_new(unsigned int size) { \
     }; \
     return arr; \
 } \
+static inline PREFIX ## mw_ ## NAME ## _array \
+PREFIX ## mw_ ## NAME ## _array_newl(unsigned int size, void* data) { \
+    PREFIX ## mw_ ## NAME ## _array arr = PREFIX ## mw_ ## NAME ## _array_new(size); \
+	if (!mw_check()) {return arr;} \
+	_mw_array_from((_mw_array*)&arr, data); \
+    return arr; \
+} \
 static inline void \
-PREFIX ## mw_ ## NAME ## _array_from(PREFIX ## mw_ ## NAME ## _array* arr, void* data) { _mw_array_from((_mw_array*)arr, data); } \
+PREFIX ## mw_ ## NAME ## _array_from(PREFIX ## mw_ ## NAME ## _array* arr, TYPE data[]) { _mw_array_from((_mw_array*)arr, data); } \
 \
 static inline void \
 PREFIX ## mw_ ## NAME ## _array_delete(PREFIX ## mw_ ## NAME ## _array* arr) { _mw_array_delete((_mw_array*)arr); } \
@@ -109,9 +116,11 @@ PREFIX ## mw_ ## NAME ## _array_delete(PREFIX ## mw_ ## NAME ## _array* arr) { _
 static inline unsigned int \
 PREFIX ## mw_ ## NAME ## _array_size(PREFIX ## mw_ ## NAME ## _array* arr) { return arr->size; } \
 \
-static inline void \
-PREFIX ## mw_ ## NAME ## _array_get(PREFIX ## mw_ ## NAME ## _array* arr, unsigned int i, TYPE* ret) { \
-    _mw_array_get((_mw_array*)arr, i, ret); \
+static inline TYPE \
+PREFIX ## mw_ ## NAME ## _array_get(PREFIX ## mw_ ## NAME ## _array* arr, unsigned int i) { \
+	TYPE ret; \
+    _mw_array_get((_mw_array*)arr, i, &ret); \
+	return ret; \
 } \
 \
 static inline void \
@@ -123,5 +132,7 @@ PREFIX ## mw_ ## NAME ## _array_set(PREFIX ## mw_ ## NAME ## _array* arr, unsign
 #define mw_define_array(NAME, TYPE) _mw_define_array_with_prefix(,NAME,TYPE)
 #define _mw_define_internal_array(NAME, TYPE) _mw_define_array_with_prefix(_,NAME,TYPE)
 
+mw_define_array(int, int)
+mw_define_array(float, float)
 
 #endif // MEOWWW_ARRAY_H
